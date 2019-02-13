fd4610bf5fd7b0958578e311c255de2d585f61f7
Hunter <ross.hunter@abbott.com>
2019-02-13 19:41:48 +0000

    add of variable and function int.

--- solution1.py
+++ solution1.py
@@ -1,3 +1,3 @@
 # solution to problem 1
-# entering of string text via use on input fuction
-input("Please enter a positive integer:  ")
+# entering of string text via use on input fuction, as it is integer, create variable i = and int for integer
+i=int(input("Please enter a positive integer:  "))
