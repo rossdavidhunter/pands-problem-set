d645d9d299ab31aac86b8d97d7f43464afb006d0
Hunter <ross.hunter@abbott.com>
2019-02-13 20:17:44 +0000

    Calculation of Result.

--- solution1.py
+++ solution1.py
@@ -1,3 +1,8 @@
 # solution to problem 1
 # entering of string text via use on input fuction, as it is integer, create variable i = and int for integer
 i=int(input("Please enter a positive integer:  "))
+
+# calculate result based on entry
+i= i * (i+1)/2
+
+print(i)
