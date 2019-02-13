72e56f7ce177811a9afe9c254806be317be77c76
Hunter <ross.hunter@abbott.com>
2019-02-13 20:21:30 +0000

    Update to calculation using //.

--- solution1.py
+++ solution1.py
@@ -2,7 +2,7 @@
 # entering of string text via use on input fuction, as it is integer, create variable i = and int for integer
 i=int(input("Please enter a positive integer:  "))
 
-# calculate result based on entry
-i= i * (i+1)/2
+# calculate result based on entry, using / will result in number been displayed as floating point, so use of // will bring to whole number
+i= i * (i+1)//2
 
 print(i)
