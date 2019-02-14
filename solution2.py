c2ca896be6fd736833967816d4b9816736fb06cb
Hunter <ross.hunter@abbott.com>
2019-02-14 16:52:10 +0000

    Checking options with days and date functions.

--- solution2.py
+++ solution2.py
@@ -1,5 +1,7 @@
 # solution to problem 2
 # to add in days of week need datetime config
-import datetime import date
+from days import datetime
+
+Days = ('Mon','Tue','Wed','Thurs','Fri')
 
 print(days.startswith('t'))
