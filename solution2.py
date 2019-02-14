9bd7ee390d6b9e0a3455f11095d27bd28713d5d2
Hunter <ross.hunter@abbott.com>
2019-02-14 16:58:40 +0000

    Change to use datetime and date.

--- solution2.py
+++ solution2.py
@@ -1,7 +1,7 @@
 # solution to problem 2
 # to add in days of week need datetime config
-from days import datetime
+from datetime import date
 
 Days = ('Mon','Tue','Wed','Thurs','Fri')
 
-print(days.startswith('t'))
+print(days.[date.weekdays(date.today())].startswith('t'))
