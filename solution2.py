2ae383873ab69bf3920590cf2429dea4f6974d98
Hunter <ross.hunter@abbott.com>
2019-02-17 12:38:41 +0000

    fixed if statement and program now working with print statements.

--- solution2.py
+++ solution2.py
@@ -5,7 +5,8 @@ from datetime import date
 # create variable with days of week
 Days = ('Mon','Tue','Wed','Thurs','Fri','Sat','Sun')
 
-if Days.[weekday(date.today())].startswith('T'):
-    print('Today does start with a T.')
+if (Days[date.weekday(date.today())].startswith('T')):
+    print('Today begins with T')
 else:
-    print('Today does not begin with a T.')
+    print('Today does not begin with T')
+  
