641c53a77fd221194dcddee27a35fdecba6b16e2
Hunter <ross.hunter@abbott.com>
2019-02-19 19:42:57 +0000

    Updates to solution 3 and 6.

--- solution3.py
+++ solution3.py
@@ -1,7 +1,7 @@
 # Solution to problem 3
 # use range functionality
 for value in range(1000,10000):
-# then say if value is dived by 6 = even number
+# then say if value is divided by 6 = even number
     if value%6 == 0: 
 
         print(value)
--- solution6.py
+++ solution6.py
@@ -1,1 +1,3 @@
 # Solution 6
+# create string with text
+sentence = ("The quick brown fox jumps over the lazy dog")
