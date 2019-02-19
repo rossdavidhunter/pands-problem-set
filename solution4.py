b034c389519fcbff8c6533677cdd5bf133eca91f
Hunter <ross.hunter@abbott.com>
2019-02-19 19:10:53 +0000

    Add of program to divide when even and multiply and add 1 when odd.

--- solution4.py
+++ solution4.py
@@ -1,3 +1,14 @@
 # Solution 4
 # Create and input
 x = int(input("Please enter a positive integer:  "))
+
+while x!= 1:
+
+    print(x)
+# if even divide by 2
+    if x % 2 == 0:
+        x = x // 2
+    else:
+# if odd multiply by 3 and add 1
+        x = 3 * x + 1
+print (x)
