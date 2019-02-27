2324803126e3043ae48e43bc0149b726369da515
Hunter <ross.hunter@abbott.com>
2019-02-27 12:46:07 +0000

    Add of final comments and clean up of code.

--- solution8.py
+++ solution8.py
@@ -1,11 +1,15 @@
+#Ross Hunter, 2019 Solution 8 use of datetime
+
+#Adapted from: https://www.pythonforbeginners.com/basics/python-datetime-time-examples and https://www.w3resource.com/python-exercises/python-basic-exercise-3.php
+
 # use of import for date and time
+import datetime as dt
 
-from datetime import datetime
-
-today = datetime.today()
+today = dt.datetime.today()
 
 # add date and time format to string using strftime
 Final = today.strftime("%A, %d %b %Y at %H:%M%p")
+
 print(Final)
 
 
