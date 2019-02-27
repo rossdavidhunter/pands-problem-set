5c2c8337e0edf333ef24b8c6b39caddb3e8f173e
Hunter <ross.hunter@abbott.com>
2019-02-27 12:30:15 +0000

    Add of final comments and clean up of code.

--- solution4.py
+++ solution4.py
@@ -1,15 +1,19 @@
-# Solution 4
+# Ross Hunter, 2019 Solution 4 use of collatz conjecture
+
+# Adapted from: https://stackoverflow.com/a/13366858
+
 # Create and input
 x = int(input("Please enter a positive integer:  "))
 
-while x!= 1:
+while x != 1:
 
     print(x)
-# if even divide by 2
+# if even divide by 2, use of // to remove decimal point and return integer
     if x % 2 == 0:
         x = x // 2
+      
     else:
+        
 # if odd multiply by 3 and add 1
-    
         x = 3 * x + 1
 print (x)
