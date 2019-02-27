036e5e5d33610a2784232fe667d2d999de6d00c7
Hunter <ross.hunter@abbott.com>
2019-02-27 11:34:50 +0000

    Add of final comments.

--- solution1.py
+++ solution1.py
@@ -1,10 +1,13 @@
-# solution to problem 1
+# Ross Hunter, 2019 Solution 1 sumupto
+
+#Adapted from: https://www.quora.com/How-do-you-find-the-sum-of-a-given-range-of-integers-in-python and GMIT course videos
+
 # entering of string text via use on input fuction, as it is integer, create variable i = and int for integer
 i=int(input("Please enter a positive integer:  "))
 
 total = 0
 
-# use of while loop
+# use of while loop and decrement i by 1
 while i > 0:
     total = total + i
     i = i - 1
