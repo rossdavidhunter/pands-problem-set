40c645774d0f86a0a45336b457380cb89275dda9
Hunter <ross.hunter@abbott.com>
2019-03-07 20:14:44 +0000

    Update to solution 9 and comments.

--- solution9.py
+++ solution9.py
@@ -1,9 +1,15 @@
-# Solution 9
+# Ross Hunter, 2019 Solution 9 secondstring
+
+# Adapted from: https://stackabuse.com/read-a-file-line-by-line-in-python/ and Lecture 7 'Open Files for Reading and Writing'
+
 # use of open() to return file object in path below
 with open("C:\\Users\\hunterd\\Desktop\\Moby Dick\\moby-dick.txt", "r") as f:
 
+# readlines will read all lines of text
     lines = f.readlines()
 
-desired_lines = [x.strip() for x in lines[1::2]]
+# use of strip() removes space between lines
+    desired_lines = [x.strip() for x in lines[1::2]]
 
 print(desired_lines)
+
