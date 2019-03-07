0c4c5271429e08ae55b2e3e03d0e6dfabd2f879a
Hunter <ross.hunter@abbott.com>
2019-03-07 20:39:02 +0000

    Completion of solution 7 and add comments.

--- solution7.py
+++ solution7.py
@@ -1,1 +1,12 @@
-# Solution 7
+# Ross Hunter, 2019 Solution 7 square root
+
+# Adapted from:https://www.programiz.com/python-programming/examples/square-root and https://www.codecademy.com/en/forum_questions/51382b87954cc276830008f9
+
+# Use of package called math
+import math
+# create variable and input for floating point number
+Num = float(input("Please enter a positive number:  "))
+
+# Use string and math sq root function to dispay output as string, %.1f displays floating point to 1 decimal place
+print("The Square Root of" ,Num, "is Approx. " '%.1f' % math.sqrt(Num))
+
