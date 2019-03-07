4786dce2c865c9fc161551136256818e55d9375e
Hunter <ross.hunter@abbott.com>
2019-03-07 19:40:22 +0000

    Completion of solution 6 and comments.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # pands-problem-set, Ross Hunter, 2019
+
+# References 
--- solution6.py
+++ solution6.py
@@ -1,7 +1,12 @@
-# Solution 6
-# create string with text and use split()
-sentence = ("The quick brown fox jumps over the lazy dog".split())
+# Ross Hunter, 2019 Solution 6 secondstring
 
-# use of enumerate function
-for i, value in enumerate(sentence):
-    print(i,value)
+#Adapted from: http://book.pythontips.com/en/latest/enumerate.html
+
+# create string with text and use split() and use of enumerate and split functions
+for i, s in enumerate ("The quick brown fox jumps over the lazy dog".split()):
+
+    # if i is odd 
+
+    if i % 2 == 1:
+
+        print(s)
